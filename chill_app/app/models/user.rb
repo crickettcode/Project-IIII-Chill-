60ab84575552850f23c35ef295641205ee05f37b
crickett <crickettcode@gmail.com>
2018-05-31 18:30:26 -0400

    get route

--- chill_app/app/models/user.rb
+++ chill_app/app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-    has_many :journal_entries, dependent: :destroy
-end
+has_many :journal_entries, dependent: :destroy
 has_many :check_ins, dependent: :destroy
 end 
