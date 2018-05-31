edb297a056a47a0863ea4c8272e43c7f94f73875
crickett <crickettcode@gmail.com>
2018-05-31 17:44:26 -0400

    controller one

--- chill_app/config/routes.rb
+++ chill_app/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
+    resources :users do
+      resources :journal_entries
+      resources :check_ins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+end 
+end 
+
+
