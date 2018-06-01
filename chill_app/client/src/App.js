98da760f5fa8a6ef5be911b4320140acb772f108
crickett <crickettcode@gmail.com>
2018-06-01 18:42:30 -0400

    profilepage

--- chill_app/client/src/App.js
+++ chill_app/client/src/App.js
@@ -1,29 +1,32 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
 import "./App.css"
 import axios from 'axios'
 import NavBar from './components/Navbar'
+import ProfilePage from './components/ProfilePage'
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
+        <Switch>
+          <div className="App">
 
-          <div>
-            <NavBar>
-              <h1>Chill</h1>
-            </NavBar>
             <div>
+              <NavBar>
+                <h1>Chill</h1>
+              </NavBar>
+              <div>
 
-              <div><Link to="/">All Journal Entries</Link></div>
+                <div><Link to="/">All Journal Entries</Link></div>
+              </div>
             </div>
+
+            <Route exact path="/" component={ProfilePage} />
+            {/* <Route path="/artist/:id" component={Artist} /> */}
+
           </div>
-
-          {/* <Route exact path="/" component={ArtistList} />
-          <Route path="/artist/:id" component={Artist} /> */}
-
-        </div>
+        </Switch>
       </Router>
     );
   }
---
+++ chill_app/client/src/components/ProfilePage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+// import styled from 'styled-components'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+// import axios from 'axios'
+
+class ProfilePage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World </h1>
+            </div>
+        );
+    }
+}
+
+export default ProfilePage;
