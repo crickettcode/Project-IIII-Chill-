32315d4230152a1af0debacb77a4ec39d77b11c9
crickett <crickettcode@gmail.com>
2018-06-01 16:29:46 -0400

    show route for checkins rails

--- chill_app/app/controllers/api/check_ins_controller.rb
+++ chill_app/app/controllers/api/check_ins_controller.rb
@@ -1,2 +1,30 @@
 class Api::CheckInsController < ApplicationController
+
+def index 
+        @check_ins = User.find(params[:user_id]).check_ins
+        render json:@check_ins
+    end 
+    
+    def show 
+        @check_ins = CheckIn.find(params[:id])
+        render json: @check_ins
+    end 
+    
+    # def create 
+    #     @user = User.find(params[:user_id])
+    #     @journal_entries = @user.journal_entries.create!(journal_entry_params)
+    #     render json: @journal_entries
+    # end 
+    
+    
+    
+    private 
+    
+    def check_ins_params 
+    
+        params.require(:check_ins).permit(:time_of_day, :mood, :hunger, :energy, :physical, :mental, :motivated, :grateful, :supported,)
+    
+    end 
+    
+
 end
--- chill_app/app/controllers/api/journal_entries_controller.rb
+++ chill_app/app/controllers/api/journal_entries_controller.rb
@@ -18,37 +18,6 @@ end
 
 
 
-# def create 
-# @user = User.create!(user_params)
-# render json:@user
-# end 
-
-# def show 
-# @user = User.find(params[:id])
-# render json:@user
-# end 
-
-# def update
-# @user = User.find(params[:id])
-# @user.update!(user_params)
-
-# render json: @user 
-# end
-
-# def destroy 
-# @user = User.find(params[:id]).delete
-
-# render status: :ok
-# end
-
-# private 
-# def user_params
-# params.require(:user).permit(:username,:email,:date_of_birth,:gender,:height,:weight)
-# end
-
-# end
-
-
 private 
 
 def journal_entry_params 
