ab14bc2bd8ac0bb776ce5df6fcc5fc4cd8957062
crickett <crickettcode@gmail.com>
2018-06-04 11:57:48 -0400

    styled 4

--- chill_app/client/src/components/Navbar.js
+++ chill_app/client/src/components/Navbar.js
@@ -5,13 +5,16 @@ import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
 class Navbar extends Component {
     render() {
         return (
-            <NavBar> <div>
+            <Top>
+                <NavBar> <div>
 
-                <h1>Chill</h1>
-                <Header><Link to="/users">ProfilePage</Link>
-                    <span />
-                    <Link to="/">Home</Link> </Header>
-            </div></NavBar>
+                    <h1>Chill</h1>
+                    <Header><Link to="/users">ProfilePage</Link>
+                        <span />
+                        <Link to="/">Home</Link> </Header>
+
+                </div></NavBar>
+            </Top>
         );
     }
 }
@@ -23,27 +26,44 @@ export default Navbar;
 const NavBar = styled.div`
 background-color: #F9D6FF;
 display:flex;
-justify-content: space-around;
-width: 100vw;
+justify-content: space-evenly;
+align-content:center;
+flex-direction:column;
+width: 80%;
+height:100%;
 color:white;
 font-size:38;
 font-weight:bold;
 font-size:20px;
+border-radius:10px;
+
+margin:0,auto;
 
 
 
-p.oblique {
+/* p.oblique {
     font-style: oblique;
-}
+} */
 
 
 a {
     color: white;
     font-size:20px;
     font-family:monospace;
+    display:flex;
+    flex-direction:row;
+    border:solid;
+    border-color:#FFCD8F;
+}
+
+
+h1 {
+    color:#8FFDFF;
+    font-variant:20px;
+    font-weight:100;
+    font-size:50px;
 }
 `
-
 
 
 const Header = styled.div`
@@ -51,4 +71,15 @@ display:flex;
 flex-direction:column;
 align-content:space-around;
 padding:30px;
+border-width:20px;
+
+
+
+`
+
+const Top = styled.div`
+background:#AAFFDE;
+padding:70px;
+display:flex;
+flex-direction:column;
 `
